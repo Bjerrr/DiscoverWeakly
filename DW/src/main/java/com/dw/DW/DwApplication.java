537a89c3a2500476980b81538c5670a2786b7210
Jens Bjerre <jensebjerre@gmail.com>
2022-12-20 11:28:09 +0100

    bean template för framtida calls

--- DW/src/main/java/com/dw/DW/DwApplication.java
+++ DW/src/main/java/com/dw/DW/DwApplication.java
@@ -2,7 +2,9 @@ package com.dw.DW;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
@@ -10,6 +12,13 @@ public class DwApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DwApplication.class, args);
+	}
+
+	//med restemplate kan man göra http calls, dom är dock i blocking miljö med det borde vara ok
+	//då är det bara att instansiera i en class och sedan köra dess metoder
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
