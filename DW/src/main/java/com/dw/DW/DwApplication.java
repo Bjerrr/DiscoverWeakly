93f6231b2e1d5ed9e513a909fd58f7c6b3ac6703
Joel Eriksson Sinclair <joeles76@gmail.com>
2022-12-20 13:34:19 +0100

    Example Get mappings for search suggestions and trips.

--- DW/src/main/java/com/dw/DW/DwApplication.java
+++ DW/src/main/java/com/dw/DW/DwApplication.java
@@ -1,11 +1,17 @@
 package com.dw.DW;
 
+import com.dw.DW.GENERATED_POJOS.JsonTrip.JsonTripRoot;
+import com.dw.DW.GENERATED_POJOS.JsonTrip.Trip;
 import com.dw.DW.fetchTrip.FetchTrip;
+import com.dw.DW.searchSuggestion.SearchSuggestion;
+import com.google.gson.Gson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -23,7 +29,31 @@ public class DwApplication {
 	@GetMapping("/trip")
 	public String trip(@RequestParam(value = "origin", defaultValue = "Göteborg Centralstation") String origin, @RequestParam(value = "destination", defaultValue = "Malmö Centralstation") String destination) {
 		FetchTrip fetchTrip = new FetchTrip();
+
 		return String.format(fetchTrip.getTrip(origin,destination));
 	}
 
+	@GetMapping("/prettyTrip")
+	public String prettyTrip(@RequestParam(value = "origin", defaultValue = "Göteborg Centralstation") String origin, @RequestParam(value = "destination", defaultValue = "Malmö Centralstation") String destination) {
+		FetchTrip fetchTrip = new FetchTrip();
+
+		String rawJson = fetchTrip.getTrip(origin, destination);
+		JsonTripRoot jsonRoot = new Gson().fromJson(rawJson, JsonTripRoot.class);
+
+		Trip trip = jsonRoot.getTrip().get(0);
+		String tripDeparture = trip.getOrigin().getTime();
+		String tripOrigin = trip.getOrigin().getName();
+		String tripDestination = trip.getDestination().getName();
+		String tripArrival = trip.getDestination().getTime();
+
+		return String.format("%s : %s ---> %s : %s", tripDeparture, tripOrigin, tripDestination, tripArrival);
+	}
+
+	@GetMapping("/suggestion")
+	public List<String> suggestion(@RequestParam(value = "query", defaultValue = "") String query) {
+		List<String> words = SearchSuggestion.loadList();
+		SearchSuggestion suggestion = new SearchSuggestion(words);
+
+		return suggestion.suggest(query);
+	}
 }
