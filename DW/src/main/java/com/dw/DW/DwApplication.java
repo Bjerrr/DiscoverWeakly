5ed365c00b6a156fc925f8c390eb6471d5eb17b8
Jens Bjerre <jensebjerre@gmail.com>
2022-12-20 10:21:35 +0100

    index.html visas nu om man startat webbservern

--- DW/src/main/java/com/dw/DW/DwApplication.java
+++ DW/src/main/java/com/dw/DW/DwApplication.java
@@ -2,8 +2,6 @@ package com.dw.DW;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,11 +10,6 @@ public class DwApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DwApplication.class, args);
-	}
-
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
 	}
 
 }
