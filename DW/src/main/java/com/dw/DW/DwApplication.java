1842a3427e869dc607058412dbebc148bb1d7884
Joel Eriksson Sinclair <joeles76@gmail.com>
2023-01-02 10:26:00 +0100

    Added /htmlTest endpoint to reah test.html. From test.html you can swap between test and test_2 pages.

--- DW/src/main/java/com/dw/DW/DwApplication.java
+++ DW/src/main/java/com/dw/DW/DwApplication.java
@@ -12,8 +12,10 @@ import com.google.gson.Gson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -32,6 +34,13 @@ public class DwApplication {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+	}
+
+	@RequestMapping("/htmlTest")
+	public ModelAndView htmlTest() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("test.html");
+		return modelAndView;
 	}
 
 	@GetMapping("/trip")
